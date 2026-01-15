7604b9d66221e6b7d83b231cfc2f48527110b620
allenres <allen.resulidze@gmail.com>
2026-01-15 14:51:21 -0800

    complete part 2 live code

--- src/Practice.java
+++ src/Practice.java
@@ -6,6 +6,12 @@ public class Practice {
         ryan.next = new Node('t', null);
         ryan.next.next = new Node('x', null); 
 
+        // expect to pritn true
+        System.out.println(contains(head, 'x'));
+        // expect to print false
+        System.out.println(contains(head, 'z'));
+
+        remove(head, 'g');
         printList(head);
     }
 
@@ -15,5 +21,29 @@ public class Practice {
             System.out.println(current.value);
             current = current.next;
         }
+    }
+
+    public static boolean contains(Node head, char toFind) {
+        while(head != null) {
+            if (head.value == toFind) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    // remove the first node that has toRemove
+    public static char remove(Node head, char toRemove) {
+        Node current = head;
+
+        while(current.next != null) {
+            if(current.next.value == toRemove) {
+                current.next = current.next.next;
+                return toRemove;
+            }
+            current = current.next;
+        }
+        return '\0';
     }
 }
